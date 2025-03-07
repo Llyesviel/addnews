3032b5468d99873a80b6ab173ce38044d32b3b78
Yulia <yuliiasaiylovskay@gmail.com>
2025-03-08 01:09:28 +0300

    обновление urls

--- server/Ad/urls.py
+++ server/Ad/urls.py
@@ -1,11 +1,22 @@
-from django.urls import path
+from django.urls import path, include, re_path
+from rest_framework import routers
 from . import views
 
+router = routers.DefaultRouter()
+
 urlpatterns = [
-    path('', views.main, name='main'),
+    path('', views.redirect_to_main, name='index'),
+    path('main/', views.main_page, name='main'),
     path('login/', views.login_view, name='login'),
     path('register/', views.register_view, name='register'),
     path('logout/', views.logout_view, name='logout'),
-    path('skip-news/', views.skip_news, name='skip_news'),
-    path('rate-news/', views.rate_news, name='rate_news'),
+    path('profile/', views.profile_view, name='profile'),
+    path('api/', include(router.urls)),
+    path('api/rate-news/', views.rate_news, name='rate_news'),
+    path('api/skip-news/', views.skip_news, name='skip_news'),
+    path('api/change-password/', views.change_password, name='change_password'),
+    path('test-404/', views.test_404, name='test_404'),
+    
+    # Перенаправление всех остальных URL на test-404
+    re_path(r'^.*$', views.redirect_to_404, name='catch_all'),
 ]
