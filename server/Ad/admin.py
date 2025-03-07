7f57e9e5a269bd1048441990548f7e499da00a1f
Yulia <yuliiasaiylovskay@gmail.com>
2025-03-07 21:27:25 +0300

    Обновление админ панели

--- server/Ad/admin.py
+++ server/Ad/admin.py
@@ -1,12 +1,20 @@
 from django.contrib import admin
-from .models import News, NewsRating
+from django.core.exceptions import ValidationError
+#import cv2
+from .models import News, AdVideo, CurrencyRate, BackgroundImage, NewsSource
+from django.contrib import messages
 
 @admin.register(News)
 class NewsAdmin(admin.ModelAdmin):
-    list_display = ('title', 'created_at')
-    search_fields = ('title', 'description')
+    list_display = ('title', 'date_published', 'source')
+    search_fields = ('title', 'source__name')
+    list_filter = ('source',)
+    fields = ('title', 'description', 'date_published', 'image', 'background_image', 'source')
 
-@admin.register(NewsRating)
-class NewsRatingAdmin(admin.ModelAdmin):
-    list_display = ('news', 'user', 'rating_type', 'created_at')
-    list_filter = ('rating_type',) 
+# def get_video_duration(file_path):
+#     video = cv2.VideoCapture(file_path)
+#     total_frames = video.get(cv2.CAP_PROP_FRAME_COUNT)
+#     fps = video.get(cv2.CAP_PROP_FPS)
+#     duration = total_frames / fps if fps != 0 else 0
+#     video.release()
+#     return duration
