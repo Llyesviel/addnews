a51df3aa1e6b227d9a1b22d98a2c2947d539584b
Yulia <yuliiasaiylovskay@gmail.com>
2025-03-07 23:46:02 +0300

    Модель курсов валют

--- server/Ad/models.py
+++ server/Ad/models.py
@@ -25,3 +25,16 @@ class News(models.Model):
 
     def __str__(self):
         return self.title
+        # Модель курсов валют
+class CurrencyRate(models.Model):
+    currency_name = models.CharField(max_length=10, verbose_name="Название валюты")
+    rate = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Курс")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата обновления")
+    symbol = models.CharField(max_length=5, verbose_name="Символ валюты", default="₽")
+
+    class Meta:
+        verbose_name = "Курс валюты"
+        verbose_name_plural = "Курсы валют"
+
+    def __str__(self):
+        return f"{self.currency_name}{self.symbol}: {self.rate}₽"
